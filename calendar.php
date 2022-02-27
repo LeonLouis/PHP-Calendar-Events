88a26efab856fe55019c758024eed602eae58537
LeonLouis <leon.lalisan@gmail.com>
2022-02-28 05:01:11 +0800

    Added Initial Files

---
+++ calendar.php
@@ -0,0 +1,222 @@
+<?php
+
+class Calendar {
+
+  // Connect to Database
+  function connect(){
+    return mysqli_connect('localhost', 'root', '', 'technical_exam');
+  }
+
+  function get_calendar($month,$year){
+    // Change Year or Month of Calendar
+    if(isset($_POST['update_calendar'])){
+      header('location:index.php?m='.$_POST['month'].'&y='.$_POST['year']);
+    }
+    // Add Events
+    if(isset($_POST['add_event'])){
+      $eventtitle = $_POST['eventtitle'];
+      $description = $_POST['description'];
+      $dateandtime = $_POST['dateandtime'];
+      $eventowner = $_POST['eventowner'];
+      $sql = "INSERT INTO events (eventtitle, description, dateandtime, eventowner) VALUES ('$eventtitle', '$description', '$dateandtime', '$eventowner')";
+      mysqli_query($this->connect(), $sql);
+      $params = $_GET;
+      if(isset($params['id'])){
+        unset($params['id']);
+      }
+      header('location:'.$_SERVER['PHP_SELF'].'?'.http_build_query($params));
+    }
+
+    // Get Data by Month and Year
+    $get_all_events = mysqli_query($this->connect(), 'SELECT *,DATE(dateandtime) as only_date FROM `events` WHERE MONTH(DATE(dateandtime)) = '.$month.' AND YEAR(DATE(dateandtime)) = '.$year.'');
+    while( $result = mysqli_fetch_assoc($get_all_events)){
+      $results[] = $result; 
+    };
+    // Get Day of Month today ( 1-31 ) 
+    $today = date('j');
+    // Today Month Number ( 01-12 )
+    $t_month = date('m');
+    // Today Year ( YYYY )
+    $t_year = date('Y');
+    // Convert Todays Date
+    $current_date_int = strtotime($t_year.'-'.$t_month.'-'.$today);
+    // Array of Days
+    $days = array( 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' );
+    // Array of Months
+    $months = array( 
+      '01' => 'January', 
+      '02' => 'February', 
+      '03' => 'March', 
+      '04' => 'April', 
+      '05' => 'May', 
+      '06' => 'June', 
+      '07' => 'July', 
+      '08' => 'August', 
+      '09' => 'Septempter', 
+      '10' => 'October', 
+      '11' => 'November', 
+      '12' => 'December'
+    );
+      // Day Number ( 0 Sunday - 6 Saturday )
+      $running_day = date('w',mktime(0,0,0,$month,1,$year));
+      // Todays Number of Days Month
+      $days_in_month = date('t',mktime(0,0,0,$month,1,$year));
+      $days_in_this_week = 1;
+      $day_counter = 0;
+      $current_data = $year.'-'.$month;
+  
+      if($month - 1 < 1){
+        $prev_month = '12';
+        $prev_year = $year - 1;
+      } else {
+        $prev_month = ( $month - 1 ) <= 9 ? '0'.$month - 1 : $month - 1;
+        $prev_year = $year;
+      }
+
+      if($month + 1 > 12){
+        $next_month = '01';
+        $next_year = $year + 1;
+      } else {
+        $next_month = ( $month + 1 ) <= 9 ? '0'.$month + 1 : $month + 1;
+        $next_year = $year;
+      }
+    ?>
+    <div class="calendar-wrapper container col-md-12">
+      <div class="row align-items-center card shadow">
+        <div class="card-body py-5">
+          <h2 class="text-center mb-4 text-uppercase">
+            <a href="<?php echo $_SERVER['PHP_SELF'].'?m='.$prev_month.'&y='.$prev_year; ?>" class="icon"><i class="fa-solid fa-angle-left"></i></a>
+            <?php echo date('F', strtotime($current_data)); ?>
+            <a href="<?php echo $_SERVER['PHP_SELF'].'?m='.$next_month.'&y='.$next_year; ?>" class="icon"><i class="fa-solid fa-angle-right"></i></a>
+          </h2>
+          <div class="calendar-btns mb-4">
+            <form method="post">
+              <select name="month" id="month">
+                <?php foreach($months as $month_key => $month_val):
+                  if($month_key == $month):
+                ?>
+                  <option value="<?php echo $month_key; ?>" selected><?php echo $month_val; ?></option>
+                <?php else: ?>
+                  <option value="<?php echo $month_key; ?>"><?php echo $month_val; ?></option>
+                <?php endif; ?>
+                <?php endforeach; ?>
+              </select>
+              <select name="year" id="year">
+                <?php for($y = 1902; $y <= 2075; $y++): 
+                  if($y == $year):
+                ?>
+                  <option value="<?php echo $y; ?>" selected><?php echo $y; ?></option>
+                <?php else: ?>
+                  <option value="<?php echo $y; ?>"><?php echo $y; ?></option>
+                <?php endif; ?>
+                <?php endfor; ?>
+              </select>
+              <button type="submit" name="update_calendar" class="btn btn-warning text-white"><span><i class="fa fa-clock"></i></span> Show</button>
+            </form>
+            <button type="button" class="btn btn-addevent" data-toggle="modal" data-target="#addEvent">Add an Event</button>
+          </div>
+          <table class="table-responsive main-calendar w-100">
+            <tr>
+              <?php foreach( $days as $day ): ?>
+                <th class="calendar-head text-center py-2"><div class="font-weight-normal"><?php echo $day; ?></div></th>
+              <?php endforeach; ?>
+            </tr>
+              <tr>
+                <?php for($x = 0; $x < $running_day; $x++): ?>
+                  <td class="day calendar-day-prev"> </td>
+                  <?php $days_in_this_week++; ?>
+                <?php endfor; ?>
+                  <?php for($list_day = 1; $list_day <= $days_in_month; $list_day++): ?>
+                    <td class="day calendar-day">
+                      <?php 
+                        $m_day = $list_day < 10 ? '0'.$list_day : $list_day; 
+                        $data_date = $year.'-'.$month.'-'.$m_day;
+                      ?>
+                      <div class="day-number" data-date="<?php echo $data_date; ?>">
+                        <span class="day-span"><?php echo $list_day; ?></span>
+                          <?php
+                            if(!empty($results)):
+                              foreach($results as $result_key => $result_val){
+                                if ( $result_val['only_date'] == $data_date ) { 
+                                  $params = $_GET;
+                                  $params['id'] = $result_val['id'];
+                                ?>
+                                  <a href="<?php echo $_SERVER['PHP_SELF'].'?'.http_build_query($params); ?>" data-id="<?php echo $result_val['id']; ?>">
+                                    <span class="icon"><i class="fa fa-calendar"></i></span>
+                                  </a>
+                                <?php
+                                }
+                              }
+                            endif;
+                          ?>
+                      </div>
+                    </td>
+                    <?php if($running_day == 6): ?>
+                      </tr>
+                      <?php if(($day_counter+1) != $days_in_month): ?>
+                          <tr class="calendar-row">
+                      <?php endif; ?>
+                      <?php 
+                        $running_day = -1;
+                        $days_in_this_week = 0;
+                      ?>
+                    <?php endif; ?>
+                    <?php $days_in_this_week++; $running_day++; $day_counter++; ?>
+                  <?php endfor; ?>
+              <?php if($days_in_this_week < 8): ?>
+                <?php for($x = 1; $x <= (8 - $days_in_this_week); $x++): ?>
+                  <td class="calendar-day-prev"> </td>
+                <?php endfor; ?>
+              <?php endif; ?>
+              </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+    <?php if(isset($_GET['id'])){  ?>
+      <div class="modal fade" id="showEvent" tabindex="-1" role="dialog" aria-labelledby="showEventTitle" aria-hidden="true">
+    <?php 
+      foreach($results as $result_key => $result_val){
+        if ( $result_val['id'] == $_GET['id'] ) {
+    ?>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="showEvent">Event Details</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="events">
+                <div class="form-group">
+                  <label>Event Title</label>
+                  <label class="form-control mb-2" ><?php echo $result_val['eventtitle']?></label>
+                </div>
+                <div class="form-group">
+                  <label>Description</label>
+                  <label class="form-control mb-2" ><?php echo $result_val['description']?></label>
+                </div>
+                <div class="form-group">
+                  <label>Date and Time</label>
+                  <label class="form-control mb-2" ><?php echo $result_val['dateandtime']?></label>
+                </div>
+                <div class="form-group">
+                  <label>Event Owner</label>
+                  <label class="form-control mb-2" ><?php echo $result_val['eventowner']?></label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    <?php
+        }
+      }
+    }
+    ?>
+    </div>
+    <?php
+  }  
+}
+
+  
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include('calendar.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Technical Exam</title>
+  </head>
+  <body>
+    <!-- Modal -->
+    <div class="modal fade" id="addEvent" tabindex="-1" role="dialog" aria-labelledby="addEventTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <form method="post">
+            <div class="modal-header">
+              <h5 class="modal-title" id="addEvent">Add Event</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <div class="events">
+                  <div class="form-group">
+                    <label>Event Title</label>
+                    <input type="text" class="form-control mb-2" name="eventtitle" autocomplete="off" required>
+                  </div>
+                  <div class="form-group">
+                    <label>Description</label>
+                    <input type="text" class="form-control mb-2" name="description" placeholder="Description" autocomplete="off" required>
+                  </div>
+                  <div class="form-group">
+                    <label>Date and Time</label>
+                    <input type="datetime-local" class="form-control mb-2" name="dateandtime" autocomplete="off" required>
+                  </div>
+                  <div class="form-group">
+                    <label>Event Owner</label>
+                    <input type="text" class="form-control mb-2" name="eventowner" placeholder="Event Owner" autocomplete="off" required>
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" name="add_event" class="btn btn-primary">Add Event</button>
+            </div>
+          </form> 
+        </div>
+      </div>
+    </div>
+
+    <?php 
+      $calendar = new Calendar();
+      $m = isset($_GET['m']) && !empty($_GET['m']) ? $_GET['m'] : date('m');
+      $y = isset($_GET['y']) && !empty($_GET['y']) ? $_GET['y'] : date('Y');
+      echo $calendar->get_calendar($m,$y); 
+    ?>
+     
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
